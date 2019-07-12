26353e643cdb4527923a9617aeadb84d9b91f321
Iskra B <ibatistapoblete@gmail.com>
2019-07-12 00:41:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -27,10 +27,10 @@ class Pokemon
     self.new(id: result[0], name: result[1], type: result[2], db: db, hp: 60)
   end
 
-  # def alter_hp(hp, db)
-  #   sql = "UPDATE pokemon SET hp = ? WHERE id = ?;"
-  #   db.execute(sql, hp, self.id)
-  # end
-  #
+  def alter_hp(hp, db)
+    sql = "UPDATE pokemon SET hp = ? WHERE id = ?;"
+    db.execute(sql, hp, self.id)
+  end
+  
 
 end
